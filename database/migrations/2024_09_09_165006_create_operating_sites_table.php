626afb2ae07e51329e568039c7295d9871e98d06
Kyrpychenko <d.kyrpychenko@mbd-team.de>
2024-09-10 08:50:35 +0000

    fix migration

--- database/migrations/2024_09_09_165006_create_operating_sites_table.php
+++ database/migrations/2024_09_09_165006_create_operating_sites_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('operating_sides', function (Blueprint $table) {
+        Schema::create('operating_sites', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('operating_sides');
+        Schema::dropIfExists('operating_sites');
     }
 };
--- database/migrations/2024_09_09_165909_create_substitutes_table.php
+++ database/migrations/2024_09_09_165909_create_substitutes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('supervisor_substitutes', function (Blueprint $table) {
+        Schema::create('substitutes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('supervisor_substitutes');
+        Schema::dropIfExists('substitutes');
     }
 };
