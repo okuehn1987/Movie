9ce80ad51561b21c142b038c9735973326f8fab0
Kyrpychenko <d.kyrpychenko@mbd-team.de>
2024-11-15 13:37:32 +0000

    fix https and new build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (!$this->app->environment('local'))
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        Model::shouldBeStrict($this->app->environment('local'));
+
         Vite::prefetch(concurrency: 3);
     }
 }
