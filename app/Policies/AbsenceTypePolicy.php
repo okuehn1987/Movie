c762a48dac2d30ef055f6a4c93f0563531bde65f
MatthesSt <m.staacken@mbd-team.de>
2024-11-28 11:22:42 +0000

    fix policy AbsenceType

--- app/Policies/AbsenceTypePolicy.php
+++ app/Policies/AbsenceTypePolicy.php
@@ -21,18 +21,44 @@ class AbsenceTypePolicy
         return null; // only if this is returned, the other methods are checked
     }
 
+    public function viewIndex(User $user): bool
+    {
+        return
+            $user->hasPermission(null, 'absenceType_permission', 'read') ||
+            $user->isSubstitutionFor()->some(fn($substitution) => $substitution->hasPermission(null, 'absenceType_permission', 'read'));
+    }
+
+    public function viewShow(User $authUser, User $user): bool
+    {
+        return
+            $authUser->id === $user->id ||
+            $authUser->id === $user->supervisor_id ||
+            $authUser->hasPermission($user, 'absenceType_permission', 'read') ||
+            $authUser->isSubstitutionFor()->some(fn($substitution) => $substitution->hasPermission($user, 'absenceType_permission', 'read'));
+    }
+
     public function create(User $user): bool
     {
-        return $user->hasPermission(null, 'absenceType_permission', 'write');
+        return
+            $user->id === $user->id ||
+            $user->id === $user->supervisor_id ||
+            $user->hasPermission(null, 'absenceType_permission', 'write') ||
+            $user->isSubstitutionFor()->some(fn($substitution) => $substitution->hasPermission(null, 'absenceType_permission', 'write'));
     }
 
     public function update(User $user, AbsenceType $absenceType): bool
     {
-        return $user->hasPermission($absenceType->user, 'absenceType_permission', 'write');
+        return
+            $user->id === $user->supervisor_id ||
+            $user->hasPermission($absenceType->user, 'absenceType_permission', 'write') ||
+            $user->isSubstitutionFor()->some(fn($substitution) => $substitution->hasPermission($absenceType->user, 'absenceType_permission', 'write'));
     }
 
     public function delete(User $user, AbsenceType $absenceType): bool
     {
-        return $user->hasPermission($absenceType->user, 'absenceType_permission', 'write');
+        return
+            $user->id === $user->supervisor_id ||
+            $user->hasPermission($absenceType->user, 'absenceType_permission', 'write') ||
+            $user->isSubstitutionFor()->some(fn($substitution) => $substitution->hasPermission($absenceType->user, 'absenceType_permission', 'write'));
     }
 }
