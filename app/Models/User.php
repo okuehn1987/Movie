af6f70d118077e0c2f6e5b45dac9adf105c7a98e
KRath87 <k.rath@mbd-team.de>
2024-12-09 15:44:22 +0000

    fixed errors in user functions with Matthes & Dennis
    (cherry picked from commit 42dda2807a0bd746b0027c1f7243fe1ed5669287)

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable
     public static $PERMISSIONS = [
         'all' => [
             ['name' => 'user_permission', 'label' => 'Mitarbeitende verwalten'],
+            ['name' => 'workLog_permission', 'label' => 'Arbeitszeiten verwalten'],
             ['name' => 'workLogPatch_permission', 'label' => 'Zeitkorrekturen verwalten'],
             ['name' => 'absence_permission', 'label' => 'Abwesenheiten verwalten'],
             ['name' => 'timeAccount_permission', 'label' => 'Zeitkonten verwalten'],
