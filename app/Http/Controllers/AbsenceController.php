692e3019e59365b84a4a82eef24f6afd9724b2e7
Kyrpychenko <d.kyrpychenko@mbd-team.de>
2024-12-17 09:33:06 +0000

    fix absence index permissions

--- app/Http/Controllers/AbsenceController.php
+++ app/Http/Controllers/AbsenceController.php
@@ -30,16 +30,17 @@ class AbsenceController extends Controller
 
         $absences = Absence::inOrganization()->where('status', 'accepted')
             ->where(fn($q) => $q->where('start', '<=', $date->copy()->endOfMonth())->where('end', '>=', $date->copy()->startOfMonth()))
-            ->with(['absenceType:id,abbreviation', 'user:id'])
+            ->with(['absenceType:id,abbreviation', 'user:id,group_id,operating_site_id,supervisor_id'])
             ->get(['id', 'start', 'end', 'absence_type_id', 'user_id', 'status'])
-            ->filter(fn($a) => $user->can('viewShow', $a));
+            ->filter(fn($a) => $user->can('viewShow', [Absence::class, $a->user]));
 
         $users = User::inOrganization()
-            ->whereIn('id', $absences->pluck('user_id')->unique())
             ->with([
                 'userWorkingWeeks:id,user_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday'
             ])
-            ->get(['id', 'first_name', 'last_name', 'supervisor_id'])->map(fn($u) => [
+            ->get(['id', 'first_name', 'last_name', 'supervisor_id', 'group_id'])
+            ->filter(fn($u) => $user->can('viewShow', [Absence::class, $u]))
+            ->map(fn($u) => [
                 ...$u->toArray(),
                 'can' => [
                     'absence' => [
