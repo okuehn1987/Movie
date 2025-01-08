ffd8bcc1c1f3ad6311adc3350a69e0b58c99d562
MatthesSt <m.staacken@mbd-team.de>
2025-01-08 14:19:34 +0000

    remove unused ternary

--- resources/js/Layouts/partials/DrawerMenu.vue
+++ resources/js/Layouts/partials/DrawerMenu.vue
@@ -19,45 +19,40 @@ import { router } from '@inertiajs/vue3';
                     value: route('workLog.index'),
                     title: 'Arbeitszeiten',
                 },
-                ...(['organization', 'operatingSite', 'group', 'user', 'timeAccountSetting'].some(m => can(m, 'viewIndex')) ||
-                can('organization', 'viewShow')
-                    ? [
-                          can('organization', 'viewShow') && {
-                              title: 'Organisation',
-                              subtitle: 'test',
-                              value: route('organization.show', { organization: $page.props.organization.id }),
-                              props: { prependIcon: 'mdi-domain', active: route().current('organization.show') },
-                          },
-                          can('operatingSite', 'viewIndex') && {
-                              props: { active: route().current('operatingSite.index'), prependIcon: 'mdi-map-marker' },
-                              value: route('operatingSite.index'),
-                              title: 'Betriebsstätten',
-                          },
-                          can('group', 'viewIndex') && {
-                              props: { active: route().current('group.index'), prependIcon: 'mdi-dots-circle' },
-                              value: route('group.index'),
-                              title: 'Abteilungen',
-                          },
-                          can('user', 'viewIndex') && {
-                              props: { active: route().current('user.index'), prependIcon: 'mdi-account-group' },
-                              value: route('user.index'),
-                              title: 'Mitarbeitende',
-                          },
-                          can('timeAccountSetting', 'viewIndex') && {
-                              props: {
-                                  active: route().current('timeAccountSetting.index'),
-                                  prependIcon: 'mdi-timer',
-                              },
-                              value: route('timeAccountSetting.index'),
-                              title: 'Arbeitszeitkonten',
-                          },
-                          can('user', 'viewIndex') && {
-                              props: { prependIcon: 'mdi-tree', active: route().current('organization.tree') },
-                              value: route('organization.tree', { organization: $page.props.organization.id }),
-                              title: 'Organigramm',
-                          },
-                      ]
-                    : []),
+                can('organization', 'viewShow') && {
+                    title: 'Organisation',
+                    subtitle: 'test',
+                    value: route('organization.show', { organization: $page.props.organization.id }),
+                    props: { prependIcon: 'mdi-domain', active: route().current('organization.show') },
+                },
+                can('operatingSite', 'viewIndex') && {
+                    props: { active: route().current('operatingSite.index'), prependIcon: 'mdi-map-marker' },
+                    value: route('operatingSite.index'),
+                    title: 'Betriebsstätten',
+                },
+                can('group', 'viewIndex') && {
+                    props: { active: route().current('group.index'), prependIcon: 'mdi-dots-circle' },
+                    value: route('group.index'),
+                    title: 'Abteilungen',
+                },
+                can('user', 'viewIndex') && {
+                    props: { active: route().current('user.index'), prependIcon: 'mdi-account-group' },
+                    value: route('user.index'),
+                    title: 'Mitarbeitende',
+                },
+                can('timeAccountSetting', 'viewIndex') && {
+                    props: {
+                        active: route().current('timeAccountSetting.index'),
+                        prependIcon: 'mdi-timer',
+                    },
+                    value: route('timeAccountSetting.index'),
+                    title: 'Arbeitszeitkonten',
+                },
+                can('user', 'viewIndex') && {
+                    props: { prependIcon: 'mdi-tree', active: route().current('organization.tree') },
+                    value: route('organization.tree', { organization: $page.props.organization.id }),
+                    title: 'Organigramm',
+                },
                 ...(can('organization', 'viewIndex')
                     ? [
                           { type: 'divider' },
