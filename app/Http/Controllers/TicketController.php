ebdee2d6b5f9bf9c031524ffe68cc0a29dc2cf80
MatthesSt <m.staacken@mbd-team.de>
2025-10-21 09:49:01 +0000

    replace status

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\Status;
 use App\Models\Customer;
 use App\Models\CustomerOperatingSite;
 use App\Models\OperatingSite;
@@ -81,7 +82,7 @@ class TicketController extends Controller
                 )
                 ->when(
                     array_key_exists('assignees', $validated) && $validated['assignees'] != null,
-                    fn($q) => $q->whereHas('assignees', fn($q2) => $q2->wherePivot('status', 'accepted')->whereIn('users.id', $validated['assignees']))
+                    fn($q) => $q->whereHas('assignees', fn($q2) => $q2->wherePivot('status', Status::Accepted)->whereIn('users.id', $validated['assignees']))
                 )
                 ->when(
                     array_key_exists('start', $validated) && array_key_exists('end', $validated) && $validated['start'] != null && $validated['end'] != null,
