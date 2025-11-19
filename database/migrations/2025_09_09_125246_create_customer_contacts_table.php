50a4ac12c48048afb892b0467595520bc20fdbff
Lemac86 <T0bsen86@web.de>
2025-11-19 10:35:16 +0000

    fix migrations

--- database/migrations/2025_09_09_125246_create_customer_contacts_table.php
+++ database/migrations/2025_09_09_125246_create_customer_contacts_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('customer_contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
             $table->string('name');
+            $table->string('occupation');
             $table->string('email')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('reference_number')->nullable();
-            $table->foreignId('organization_id')->constrained()->onDelete('cascade');
+            $table->string('phone_number')->nullable();
+            $table->string('mobile_number')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('customer_contacts');
     }
 };
