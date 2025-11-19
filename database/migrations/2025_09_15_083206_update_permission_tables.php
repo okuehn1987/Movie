6b5a0441b84decf2f359860380be598984f7f58e
Lemac86 <T0bsen86@web.de>
2025-11-19 11:07:48 +0000

    fix migrations

--- database/migrations/2025_09_15_083206_update_permission_tables.php
+++ database/migrations/2025_09_15_083206_update_permission_tables.php
@@ -13,20 +13,17 @@ return new class extends Migration
     {
         Schema::table('organization_users', function (Blueprint $table) {
             $table->enum('ticket_permission', ['read', 'write'])->nullable();
-            $table->enum('ticket_accounting_permission', ['read', 'write'])->nullable();
             $table->enum('customer_permission', ['read', 'write'])->nullable();
         });
 
         Schema::table('operating_site_users', function (Blueprint $table) {
             $table->enum('ticket_permission', ['read', 'write'])->nullable();
-            $table->enum('ticket_accounting_permission', ['read', 'write'])->nullable();
             $table->enum('customer_permission', ['read', 'write'])->nullable();
             $table->enum('absenceType_permission', ['read', 'write'])->nullable();
         });
 
         Schema::table('group_users', function (Blueprint $table) {
             $table->enum('ticket_permission', ['read', 'write'])->nullable();
-            $table->enum('ticket_accounting_permission', ['read', 'write'])->nullable();
             $table->enum('customer_permission', ['read', 'write'])->nullable();
             $table->enum('absenceType_permission', ['read', 'write'])->nullable();
         });
@@ -39,18 +36,15 @@ return new class extends Migration
     {
         Schema::table('organization_users', function (Blueprint $table) {
             $table->dropColumn('ticket_permission');
-            $table->dropColumn('ticket_accounting_permission');
         });
 
         Schema::table('operating_site_users', function (Blueprint $table) {
             $table->dropColumn('ticket_permission');
-            $table->dropColumn('ticket_accounting_permission');
             $table->dropColumn('absenceType_permission');
         });
 
         Schema::table('group_users', function (Blueprint $table) {
             $table->dropColumn('ticket_permission');
-            $table->dropColumn('ticket_accounting_permission');
             $table->dropColumn('absenceType_permission');
         });
     }
