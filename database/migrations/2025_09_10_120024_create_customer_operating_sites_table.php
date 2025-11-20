bf8661255faa6c38fde72bbea40398831cc425bf
Kyrpychenko <d.kyrpychenko@mbd-team.de>
2025-11-20 08:35:51 +0000

    fix migration

--- database/migrations/2025_09_10_120024_create_customer_operating_sites_table.php
+++ database/migrations/2025_09_10_120024_create_customer_operating_sites_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('customer_id')->constrained()->onDelete('cascade');
             $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('phone_number')->nullable();
             $table->timestamps();
         });
     }
