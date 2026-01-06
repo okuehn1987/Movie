7eac90e5cc4d602ac5808ab5eb9555083af6953b
MatthesSt <m.staacken@mbd-team.de>
2026-01-06 14:27:08 +0000

    fix: local lazy loading issue

--- routes/console.php
+++ routes/console.php
@@ -59,7 +59,7 @@ Schedule::call(function () {
 })->name('dailyWorkLogCalculation')->dailyAt("00:00");
 
 Schedule::call(function () {
-    foreach (User::with('operatingSite')->get() as $user) {
+    foreach (User::with('operatingSite.currentAddress')->get() as $user) {
         $year = Carbon::now()->subYear();
         $usedDaysData = $user->usedLeaveDaysForYear($year);
         $days = isset($usedDaysData[$year->year]) ? $usedDaysData[$year->year]['accepted'] : 0;
