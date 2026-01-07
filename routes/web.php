6555c56fb8a363329f025ec20697aca410ba6b3f
Lemac86 <T0bsen86@web.de>
2026-01-07 12:13:24 +0000

    refactor: routing to onePage adjusted

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Spatie\Honeypot\ProtectAgainstSpam;
 
-Route::get('/one', function () {
-    return Inertia::render('OnePage/Onepage', ['honeypot' => app(\Spatie\Honeypot\Honeypot::class),]);
-})->name('one');
+foreach (['hr-tools-and-assistants.de', 'tide-hrta.de', 'flow-hrta.de', 'one.localhost'] as $domain) {
+    Route::domain($domain)->group(function () {
+        Route::get('/', function () {
+            return Inertia::render('OnePage/Onepage', ['honeypot' => app(\Spatie\Honeypot\Honeypot::class),]);
+        })->name('one');
+    });
+}
 
 Route::post('/contact', [ContactController::class, 'store'])
     ->name('contact.store')
