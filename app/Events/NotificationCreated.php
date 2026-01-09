73f956f5689cf0e7c2870f48dbe863f1e4742fb5
KRath87 <k.rath@posteo.de>
2026-01-09 10:54:38 +0000

    feat: adds event

---
+++ app/Events/NotificationCreated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+    private int $userId;
+    private int $unreadMessages;
+
+    public function __construct( User $user, int $unreadMessages)
+    {
+        $this->userId = $user->id;
+        $this->unreadMessages = $unreadMessages;
+    }
+
+    public function broadcastOn(): Channel
+    {
+        return new Channel("notification.{$this->userId}");
+    }
+    
+    public function broadcastAs()
+    {
+        return 'NotificationCreated';
+    }
+
+}
