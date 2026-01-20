0a7bf25c31511b4245570e1a723fdffbee48a546
MatthesSt <m.staacken@mbd-team.de>
2026-01-20 16:24:29 +0000

    fix: websocket connection host

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -22,10 +22,10 @@ import { configureEcho } from '@laravel/echo-vue';
 configureEcho({
     broadcaster: 'reverb',
     key: import.meta.env['VITE_REVERB_APP_KEY'],
-    wsHost: import.meta.env['VITE_REVERB_HOST'],
+    wsHost: window.location.hostname,
     wsPort: import.meta.env['VITE_REVERB_PORT'],
     wssPort: import.meta.env['VITE_REVERB_PORT'],
-    forceTLS: (import.meta.env['VITE_REVERB_SCHEME'] ?? 'https') === 'https',
+    forceTLS: false,
     enabledTransports: ['ws', 'wss'],
 });
 
